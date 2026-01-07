ca2b8fb46a9cdfbab68d2909562d5add5516052d
Aqdar Ahmed <Trainee23@orionvtech>
2026-01-07 21:46:52 +0200

    fix bugs

--- xcelium.d/run.lnx8664.24.03.d/worklib_-3.ts
+++
@@ -1,2 +0,0 @@
-1709675774 /usr/cadence/installs/xcelium/XCELIUM2403/tools.lnx86/methodology/UVM/CDNS-1.1d/sv/src/uvm_pkg.sv
-1709675773 /usr/cadence/installs/xcelium/XCELIUM2403/tools.lnx86/methodology/UVM/CDNS-1.1d/additions/sv/cdns_uvm_pkg.sv
