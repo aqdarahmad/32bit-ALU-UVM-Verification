9b8189ae25d70e3eb409e9d7eb6c665d50a6af53
aqdarahmad <a.abusufyan@student.aaup.edu>
2026-01-08 13:51:49 +0200

    New (#9)
    * edits uvm components
    
    * fix bugs
    
    * fix bugs
    
    * fix bugs
    
    * fix bugs
    
    * fix bugs
    
    * fix bugs
    
    * update xrun history
    
    * update xrun history
    
    * update xrun history
    
    * update xrun history
    
    ---------
    
    Co-authored-by: Aqdar Ahmed <Trainee23@orionvtech>

--- verisium_debug_logs/history.py
+++ verisium_debug_logs/history.py
@@ -1,10 +1,16 @@
 #########################################################################
 # Verisium Debug version 25.09.081-a (Built on 2025-09-29T10:20:10Z)
+
+# history.py generated at 2026/01/08 13:50:46
+# host: orionvtech
+# port: 44809
+# launch command: indago -connect dc:orionvtech.orionvtech.com:45875 -interactive
+
 # history.py generated at 2026/01/07 20:46:57
 # host: orionvtech
 # port: 39587
 # launch command: indago -connect dc:orionvtech.orionvtech.com:54783 -interactive
-# #########################################################################
+
 import time, os, sys
 if 'self' not in globals():
     from verisium import *
@@ -12,11 +18,19 @@ if 'self' not in globals():
     self = VerisiumDebugServer(VerisiumDebugArgs(
         is_gui=True,
         is_launch_needed=True,
+
+        port=44809,
+        extra_args='-connect dc:orionvtech.orionvtech.com:45875 -interactive'
+    ))
+
+# Verisium: Attempting to connect to Verisium server on host: localhost, port: 44809
+
         port=39587,
         extra_args='-connect dc:orionvtech.orionvtech.com:54783 -interactive'
     ))
 
 # Verisium: Attempting to connect to Verisium server on host: localhost, port: 39587
+
 # Verisium: **************************************************************************************
 # Verisium: *****                        Verisium version 25.09.081-a                        *****
 # Verisium: *****                 NOTE: Some API features are Beta quality.                  *****
